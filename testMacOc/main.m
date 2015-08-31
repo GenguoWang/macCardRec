3d0aab1fb99d442e924e98141a9ff5d7122d4379
Kingo Wang <kingo@SpringdeMacBook-Pro.local>
2015-08-31 21:15:59 +0800

    Initial Commit

---
+++ testMacOc/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  testMacOc
+//
+//  Created by Kingo Wang on 15/8/31.
+//  Copyright (c) 2015年 Kingo Wang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
